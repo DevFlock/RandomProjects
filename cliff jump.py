fa450afc725ad94ddd32b750a91f48d941f2c938
DevFlock <37045958+DevFlock@users.noreply.github.com>
2020-08-20 23:38:02 +0100

    Update cliff jump.py

--- cliff jump.py
+++ cliff jump.py
@@ -3,5 +3,5 @@
 
     print("The answer is: ", fall_in_feet*0.3048, "meters.")
 
-    if input("Do you want to run again? (y/n)") == "n":
+    if input("Do you want to run again? (y/n)\n") == "n":
         exit(print("\n \n Hope to see you again! \n \n"))
